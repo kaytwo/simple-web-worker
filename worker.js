21ddab416441d79ca886e6d3406081d604381a47
Chris Kanich <ckanich@uic.edu>
2020-07-14 11:25:45 -0500

    count to a BILLION

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ onmessage = function (e) {
   console.log("Worker: Message received from main script");
   let ctr = 0;
   while (true) {
-    for (let i = 0; i < 1000000; i++) {
+    for (let i = 0; i < 1000000000; i++) {
       ctr += 1
     }
     postMessage(`I counted to ${ctr} by time ${Date.now()}`);
