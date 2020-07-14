903a8dbace8470f756fe97c5f626b938257f7bb3
Chris Kanich <ckanich@uic.edu>
2020-07-14 11:11:30 -0500

    oops

--- worker.js
+++ worker.js
@@ -2,8 +2,7 @@ onmessage = function (e) {
   console.log("Worker: Message received from main script");
   let ctr = 0;
   while (true) {
-    for (let i = ctr; i < ctr + 1000000; i++) {
-      postMessage(`I counted to ${i} by time ${Date.now()}`);
-    }
+    for (let i = ctr; i < ctr + 1000000; i++) {}
+    postMessage(`I counted to ${i} by time ${Date.now()}`);
   }
 };
