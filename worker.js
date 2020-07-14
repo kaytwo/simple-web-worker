dde78f2010d47c5f8281a965ddb0469e27484ffc
Chris Kanich <ckanich@uic.edu>
2020-07-14 11:22:29 -0500

    count

--- worker.js
+++ worker.js
@@ -2,7 +2,9 @@ onmessage = function (e) {
   console.log("Worker: Message received from main script");
   let ctr = 0;
   while (true) {
-    for (let i = ctr; i < ctr + 1000000; i++) {}
-    postMessage(`I counted to ${i} by time ${Date.now()}`);
+    for (let i = 0; i < 1000000; i++) {
+      ctr += 1
+    }
+    postMessage(`I counted to ${ctr} by time ${Date.now()}`);
   }
 };
