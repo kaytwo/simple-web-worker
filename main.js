20676f9c7659d3e179dc3bbe2299e29abb6bf139
Chris Kanich <ckanich@uic.edu>
2020-07-14 11:15:09 -0500

    forgot callback

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const result = document.querySelector('.result');
 
 if (window.Worker) {
 	const myWorker = new Worker("worker.js");
-
+	myWorker.onmessage = function(e) {
+		result.textContent = e.data;
+		console.log('Message received from worker');
+	}
 	myWorker.postMessage("GO");
 } else {
 	console.log('Your browser doesn\'t support web workers.')
